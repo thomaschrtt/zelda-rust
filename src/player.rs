6d753a357e9c25d832a954b90ae6659cc991abac
itchigo16 <kuma.itchigo@gmail.com>
2023-08-11 14:16:45 +0200

    added facing direction enum added sprite changing if sprint / walk / stand

--- src/player.rs
+++ src/player.rs
@@ -2,12 +2,19 @@ use bevy::prelude::*;
 use crate::constants::*;
 use crate::collisions::*;
 
+enum PlayerFacingDirection {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
-        .add_systems(Update, (player_move, player_facing_direction, update_player_pos));
+            .add_systems(Update, (player_move, update_player_pos, player_facing_direction, update_player_sprite_moving));
     }
 }
 
@@ -16,11 +23,13 @@ impl Plugin for PlayerPlugin {
 pub struct Player {
     x: i32,
     y: i32,
+    facing_direction: PlayerFacingDirection,
+    sprinting: bool,
 }
 
 impl Player {
     pub fn new() -> Self {
-        Self { x: 0, y: 0 }
+        Self { x: 0, y: 0, facing_direction: PlayerFacingDirection::Right, sprinting:false }
     }
 }
 
@@ -43,15 +52,17 @@ fn player_move(
 ) {
 
     let player_speed: i32;
+    let mut player = player_query.single_mut();
 
     if keyboard_input.pressed(KeyCode::ShiftLeft) {
+        player.sprinting = true;
         player_speed = 2;
 
     } else {
+        player.sprinting = false;
         player_speed = 1;
     }
 
-    let mut player = player_query.single_mut();
 
     let left_boundary = -((WINDOW_SIZE / 2.0) - (PLAYER_HITBOX_WIDTH / 2.)) as i32;
     let right_boundary = -left_boundary;
@@ -85,26 +96,56 @@ fn player_move(
 
 fn player_facing_direction(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>,
+    mut query: Query<&mut Player>,
 ) {
     let mut player = query.single_mut();
     if keyboard_input.pressed(KeyCode::Left) {
-        player.scale.x = -PLAYER_SPRITE_SCALE;
+        player.facing_direction = PlayerFacingDirection::Left;
     }
     if keyboard_input.pressed(KeyCode::Right) {
-        player.scale.x = PLAYER_SPRITE_SCALE;
+        player.facing_direction = PlayerFacingDirection::Right;
+    }
+    if keyboard_input.pressed(KeyCode::Up) {
+        player.facing_direction = PlayerFacingDirection::Up;
+    }
+    if keyboard_input.pressed(KeyCode::Down) {
+        player.facing_direction = PlayerFacingDirection::Down;
+    }
+}   
+
+
+fn update_player_sprite_moving(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Player, &mut TextureAtlasSprite)>,
+) {
+    let mut player = query.single_mut();
+    if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::Down) {
+        if player.0.sprinting {
+            player.1.index = 24;
+        }
+        else {
+            player.1.index = 26;
+        }
+    }
+    else {
+        player.1.index = 0;
     }
 }
-
 
 fn update_player_pos(
     mut query: Query<(&mut Player, &mut Transform)>,
 ) {
-    let mut player = query.single_mut();
-    let x = player.0.x as f32;
-    let y = player.0.y as f32;
-    player.1.translation.x = x;
-    player.1.translation.y = y;
+
+    let (player, mut sprite) = query.single_mut();
+    let x = player.x as f32;
+    let y = player.y as f32;
+    sprite.translation.x = x;
+    sprite.translation.y = y;
+    match player.facing_direction {
+        PlayerFacingDirection::Left => sprite.scale.x = -PLAYER_SPRITE_SCALE,
+        PlayerFacingDirection::Right => sprite.scale.x = PLAYER_SPRITE_SCALE,
+        _ => {}
+    }
 }
 
 fn spawn_player(mut commands: Commands, 
@@ -113,7 +154,7 @@ fn spawn_player(mut commands: Commands,
     {
 
     let texture_handle = asset_server.load("player.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(PLAYER_SPRITE_SIZE, PLAYER_SPRITE_SIZE), 4, 4, Some(Vec2::new(0., 0.)), Some(Vec2::new(0., 0.)));
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(PLAYER_SPRITE_SIZE, PLAYER_SPRITE_SIZE), 8, 9, Some(Vec2::new(0., 0.)), Some(Vec2::new(0., 0.)));
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     let player: Player = Player::new();
